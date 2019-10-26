101a0e7da9929eb62fd2ad3153766a874a0fb384
neronicolo <neronicolo@gmail.com>
2019-10-26 16:18:55 -0700

    rough layout done

--- label_me.py
+++ label_me.py
@@ -11,7 +11,7 @@ import check
 # Get image file path
 dir_path = check.path_check(Path.home()/'programming/data/watch_bot/')
 df = pd.read_csv(dir_path/'wfc_file_attribs.csv')
-file_path = dir_path/df['name'][11]
+file_path = dir_path/df['name'][666]
 # Open and resize image
 img = Image.open(file_path)
 img.thumbnail((500, 500), Image.ANTIALIAS)
@@ -33,32 +33,40 @@ root.rowconfigure(0, weight=1)
 img = ImageTk.PhotoImage(img)
 ttk.Label(mainframe, image=img).grid(row=0, column=0, rowspan=40)
 
-ttk.Label(mainframe, text="Watch face visibility:").grid(row=0, column=1, sticky=(W, N))
+ttk.Label(mainframe, text="Watch face visibility:").grid(row=34, column=1, sticky=(W, N))
 watch_face_vis = IntVar()
-ttk.Radiobutton(mainframe, text="1", variable=watch_face_vis, value=1).grid(row=0, column=2, sticky=(W, N))
-ttk.Radiobutton(mainframe, text="0.5", variable=watch_face_vis, value=0.5).grid(row=0, column=3, sticky=(W, N))
-ttk.Radiobutton(mainframe, text="0", variable=watch_face_vis, value=0).grid(row=0, column=4, sticky=(W, N))
+ttk.Radiobutton(mainframe, text="1", variable=watch_face_vis, value=1).grid(row=34, column=2, sticky=(W, N))
+ttk.Radiobutton(mainframe, text="0.5", variable=watch_face_vis, value=0.5).grid(row=34, column=3, sticky=(W, N))
+ttk.Radiobutton(mainframe, text="0", variable=watch_face_vis, value=0).grid(row=34, column=4, sticky=(W, N))
 
-ttk.Label(mainframe, text="Composition quality:").grid(row=1, column=1, sticky=(W, N))
-ttk.Radiobutton(mainframe, text="1").grid(row=1, column=2, sticky=(W, N))
-ttk.Radiobutton(mainframe, text="0.5").grid(row=1, column=3, sticky=(W, N))
-ttk.Radiobutton(mainframe, text="0").grid(row=1, column=4, sticky=(W, N))
+ttk.Label(mainframe, text="Composition quality:").grid(row=35, column=1, sticky=(W, N))
+comp_quality = IntVar()
+ttk.Radiobutton(mainframe, text="1", variable=comp_quality, value=1).grid(row=35, column=2, sticky=(W, N))
+ttk.Radiobutton(mainframe, text="0.5", variable=comp_quality, value=0.5).grid(row=35, column=3, sticky=(W, N))
+ttk.Radiobutton(mainframe, text="0", variable=comp_quality, value=0).grid(row=35, column=4, sticky=(W, N))
 
-ttk.Label(mainframe, text="Lighting quality:").grid(row=2, column=1, sticky=(W, N))
-ttk.Radiobutton(mainframe, text="1").grid(row=2, column=2, sticky=(W, N))
-ttk.Radiobutton(mainframe, text="0").grid(row=2, column=3, sticky=(W, N))
+ttk.Label(mainframe, text="Lighting quality:").grid(row=36, column=1, sticky=(W, N))
+lght_quality = IntVar()
+ttk.Radiobutton(mainframe, text="1", variable=lght_quality, value=1).grid(row=36, column=2, sticky=(W, N))
+ttk.Radiobutton(mainframe, text="0", variable=lght_quality, value=0).grid(row=36, column=3, sticky=(W, N))
 
-ttk.Label(mainframe, text="Image quality:").grid(row=3, column=1, sticky=(W, N))
-ttk.Radiobutton(mainframe, text="1").grid(row=3, column=2, sticky=(W, N))
-ttk.Radiobutton(mainframe, text="0").grid(row=3, column=3, sticky=(W, N))
+ttk.Label(mainframe, text="Image quality:").grid(row=37, column=1, sticky=(W, N))
+img_quality = IntVar()
+ttk.Radiobutton(mainframe, text="1", variable=img_quality, value=1).grid(row=37, column=2, sticky=(W, N))
+ttk.Radiobutton(mainframe, text="0", variable=img_quality, value=0).grid(row=37, column=3, sticky=(W, N))
 
-#ttk.Button(mainframe, text="<<").grid(row=8, column=1)
-#ttk.Button(mainframe, text=">>").grid(row=8, column=2)
-#ttk.Button(mainframe, text="Save").grid(row=9, column=1)
-#ttk.Button(mainframe, text="Exit").grid(row=9, column=2)
-# TODO: Radionbuttons
+ttk.Button(mainframe, text="<<").grid(row=38, column=1, columnspan=1, sticky=())
+ttk.Button(mainframe, text=">>").grid(row=38, column=2, columnspan=3, sticky=())
+#ttk.Button(mainframe, text="Save").grid(row=39, column=1, columnspan=2, sticky=(W))
+#ttk.Button(mainframe, text="Exit").grid(row=39, column=3, columnspan=2, sticky=(E))
+#ttk.Label(mainframe, text="Status bar", relief=SUNKEN).grid(row=40, column=4, columnspan=4, sticky=(E))
 # TODO: Status bar
 
+# Walks through all of the widgets in mainframe and add padding around each
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+# Style
+s = ttk.Style()
+s.theme_use("clam")
 
 root.mainloop()
