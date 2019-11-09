4e1c6aedbd71f20c35b889d1e6553dd60d7550a7
neronicolo <neronicolo@gmail.com>
2019-11-09 10:41:38 -0800

    adding kwargs to __init__

--- wfc_image_viewer.py
+++ wfc_image_viewer.py
@@ -1,32 +1,49 @@
 import collections
 import tkinter as tk
+from tkinter import ttk
 from pathlib import Path
 
 import pandas as pd
 from PIL import Image, ImageTk
 
 import check
+# TODO: Add title
+# TODO: Key mappings/events
+# TODO: Separate status bar by line. Maybe add file name on status West side
+# TODO: Revrite __init__ to look like tkinter_example.py
+# TODO: Add docstrings
+# TODO: How to define **kwargs
 
-class ImageViewer(tk.Frame):
-    def __init__(self, dir_path, file_name, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
-        self.dir_path = dir_path
-        self.file_name = file_name
+class Application(tk.ttk.Frame):
+    def __init__(self, *args, **kwargs):
+        DIR_PATH = kwargs['dir_path']
+        FILE_NAME = kwargs['file_name']
+
+        # remove kwargs from passing to tk.ttk.Frame
+        for e in ['dir_path', 'file_name']:
+            kwargs.pop(e)
+        
+        tk.ttk.Frame.__init__(self, *args, **kwargs)
+
+        self.dir_path = DIR_PATH
+        self.file_name = FILE_NAME
+        self.df = pd.read_csv(self.dir_path/self.file_name)
         self.start_index = -1
-        self.size = (500, 500)
+        self.size = (480, 480)
         self.layout()
         self.display_next()
     
+    def total_items(self):
+        return self.df['name'].size
+        
     def image_path(self):
-        df = pd.read_csv(self.dir_path/file_name)
-        img_name = df['name'][self.start_index]
+        img_name = self.df['name'][self.start_index]
         img_path = self.dir_path/img_name
         return img_path
 
     def image_resize(self):
         img = Image.open(self.image_path())
         img.thumbnail(self.size, Image.ANTIALIAS)
-
         bg = Image.new('RGBA', self.size, (255, 255, 255, 0))
         bg.paste(img, (int((self.size[0] - img.size[0]) / 2), int((self.size[1] - img.size[1]) / 2)))
         return bg
@@ -36,46 +53,52 @@ class ImageViewer(tk.Frame):
         img = ImageTk.PhotoImage(img)
         self.img_label.configure(image=img)
         self.img_label.image = img
+        self.status_label.configure(text=f"({self.start_index + 1} / {self.total_items() - 1})")
 
     def layout(self):
-        self.img_label = tk.Label(self)
+        self.img_label = ttk.Label(self)
         self.img_label.grid(row=0, column=0, columnspan=2)
-        tk.Button(self, text="Previous", command=self.display_previous).grid(row=1, column=0, sticky=('E'))
-        tk.Button(self, text="Next", command=self.display_next).grid(row=1, column=1, sticky=('W'))
+        self.status_label = ttk.Label(self)
+        self.status_label.grid(row=2, column=0, columnspan=2, sticky=('E'))
+        ttk.Button(self, text="Previous", command=self.display_previous).grid(row=1, column=0, sticky=('E'))
+        ttk.Button(self, text="Next", command=self.display_next).grid(row=1, column=1, padx=5, pady=5, sticky=('W'))
 
     def display_next(self):
-        self.start_index += 1
-        self.image_path()
+        try:
+            self.start_index += 1
+            self.image_path()
+        except KeyError:
+            self.start_index = -1 # reset index beginning
+            self.display_next()
+            return
         self.image_load()
 
     def display_previous(self):
-        self.start_index -= 1
-        self.image_path()
+        try:
+            self.start_index -= 1
+            self.image_path()
+        except KeyError:
+            self.start_index = -1 # reset index to beginning
+            self.display_next()
+            return
         self.image_load()
 
-    # TODO: Put duplicate code into one func and call it inside other func
-    # TODO: Add padding around widgets recursively
-
-def main(dir_path, file_name):
+def main(DIR_PATH, FILE_NAME):
     root = tk.Tk()
-    root.title("Image Viewer")
 
-    app = ImageViewer(dir_path, file_name)
+    app = Application(root, dir_path=DIR_PATH, file_name=FILE_NAME, padding=5)
     app.grid(row=0, column=0)
-    for child in app.winfo_children(): child.grid_configure(padx=2, pady=2)
-    
-    # start event loop
-    root.lift()
-    root.attributes('-topmost',True)
-    root.after_idle(root.attributes,'-topmost',False)
+    #for child in app.winfo_children(): child.grid_configure(padx=2, pady=2)
+    #tk.ttk.Style().theme_use("clam")
 
+    root.title("Image Viewer" + " - ")
     root.mainloop()
 
 if __name__ == "__main__":
 
     # Path to csv file
-    dir_path = Path.home()/'programming/data/watch_bot/'
-    file_name = 'wfc_file_attribs.csv'
-    check.path_check(dir_path/file_name)
+    d = Path.home()/'programming/data/watch_bot/'
+    f = 'wfc_file_attribs.csv'
+    check.path_check(d/f)
 
-    main(dir_path, file_name)
+    main(d, f)
