4746de4c3a18af1fb6963d9e2006eb45ef8839dc
neronicolo <neronicolo@gmail.com>
2019-11-22 19:17:31 -0800

    storing values to dataframe

--- wfc_image_labeler.py
+++ wfc_image_labeler.py
@@ -13,8 +13,7 @@ import check
 #           self.a=3
 #           self.b=4
 #           self.c=6
-# TODO: set radiobuttons initial value to 0,
-#       get radiobutton value before moving to next image,
+# TODO: get radiobutton value before moving to next image,
 #       store captured value into dataframe
 #       reset value to 0
 #       For previous button set to read value from data frame and writing will be handled with next button
@@ -38,40 +37,45 @@ class Application(tk.Frame):
 
         self.dir_path = dir_path
         self.file_name = file_name
-        self.df = pd.read_csv(self.dir_path/self.file_name)
         self._index = -1
         self.size = (520, 520)
-        self.wfv_var = tk.DoubleVar(value=0)
-        self.cq_var = tk.DoubleVar(value=0)
-        self.lq_var = tk.DoubleVar(value=0)
-        self.iq_var = tk.DoubleVar(value=0)
+        self.df = pd.read_csv(self.dir_path/self.file_name)
+        # we don't need this since every time we start a program it will reset previously saved values
+        #self.df['watch_face_visibility'] = -1
+        #self.df['composition_quality'] = -1
+        #self.df['light_quality'] = -1
+        #self.df['image_quality'] = -1
+        self.wfv_var = tk.DoubleVar(value=-1)
+        self.cq_var = tk.DoubleVar(value=-1)
+        self.lq_var = tk.DoubleVar(value=-1)
+        self.iq_var = tk.DoubleVar(value=-1)
         self.layout()
         self.display_next()
 
     def layout(self):
-        # image
+        # image label
         self.img_label = tk.Label(self)
         self.img_label.grid(row=0, column=0, rowspan=40)
 
-        # radiobuttons and labelframe
-        s = {"lf_txt":["Watch Face Visibility:","Composition quality:","Lighting quality:","Image quality:"],
-            "rb_var":[self.wfv_var, self.cq_var, self.lq_var, self.iq_var],
-            "rb_txt":["1", "0.5", "0"],
-            "rb_val":[1, 0.5, 0]}
+        # radiobuttons(rb) and labelframe(lf)
+        s = {'lf_txt':["Watch Face Visibility:","Composition quality:","Lighting quality:","Image quality:"],
+            'rb_var':[self.wfv_var, self.cq_var, self.lq_var, self.iq_var],
+            'rb_txt':["1", "0.5", "0"],
+            'rb_val':[1, 0.5, 0]}
 
-        for i, text in enumerate(s["lf_txt"]):
+        for i, text in enumerate(s['lf_txt']):
             label_frame = tk.LabelFrame(self, text=text, padx=5, pady=5)
             label_frame.grid(row=i, column=1, columnspan=3, sticky=('W'))
 
-            for j, value in enumerate(s["rb_val"]):
-                radiobutton = tk.Radiobutton(label_frame, text=s["rb_txt"][j], variable=s["rb_var"][i], value=value)
+            for j, value in enumerate(s['rb_val']):
+                radiobutton = tk.Radiobutton(label_frame, text=s['rb_txt'][j], variable=s['rb_var'][i], value=value)
                 radiobutton.grid(row=i, column=j+1, padx=5) 
     
         # previous, next button
-        self.next_button = tk.Button(self, text="Previous", command=self.display_previous)
-        self.next_button.grid(row=38, column=1, columnspan=2, sticky=('E'))
-        self.previous_button = tk.Button(self, text="Next", command=self.display_next)
-        self.previous_button.grid(row=38, column=3, sticky=('W'))
+        self.previous_button = tk.Button(self, text="Previous", command=self.display_previous)
+        self.previous_button.grid(row=38, column=2, columnspan=1, sticky=('E'))
+        self.next_button = tk.Button(self, text="Next", command=self.display_next)
+        self.next_button.grid(row=38, column=3, sticky=('W'))
         return
 
     def image_resize(self, image_path):
@@ -82,7 +86,6 @@ class Application(tk.Frame):
         return new_img
 
     def display_next(self):
-        print(self.wfv_var.get())
         self._index += 1
         try:
             img_path = self.dir_path/self.df['name'][self._index]
@@ -96,6 +99,19 @@ class Application(tk.Frame):
         self.img_label.image = photoimage
         self.master.title(img_path.name)
 
+        # TODO: Look at TODO #2
+        d = {'watch_face_visibility':self.wfv_var,
+            'composition_quality':self.cq_var,
+            'light_quality':self.lq_var,
+            'image_quality':self.iq_var}
+
+        for k,v in d.items():
+            val = v.get()
+            self.df.loc[self._index-1, k]= val
+            v.set(-1)
+            print(f"{k} --> {self.df.loc[self._index-1, k]}, {self.df.loc[self._index, 'name']}")
+     
+    # TODO: modify self._index of display_next() with decorator, reuse func() since only one var is different
     def display_previous(self):
         self._index -= 1
         try:
@@ -117,6 +133,9 @@ class Application(tk.Frame):
             self.display_previous()
         #print(event.keysym)
 
+    def store_and_reset(self):
+        pass
+
 if __name__ == "__main__":
 
     # Path to csv file
@@ -127,4 +146,4 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = Application(master=root, dir_path=d, file_name=f)
  
-    root.mainloop()
+    #root.mainloop()
