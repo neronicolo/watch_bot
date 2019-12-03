88fb2acb7a95d2fa40b2480cb0924bf76369191d
neronicolo <neronicolo@gmail.com>
2019-12-03 11:09:11 -0800

    moved importing_func_test.py to snadbox folder

--- importing_func_test.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import check
-import regex
-
-folder_path = check.path_check(Path.home()/"programming/data/chrono24/")
-file_iterator = check.gen_check(folder_path.rglob('*.jpg'),100)
-
-for file in file_iterator:
-    regex.replace_name(file.stem, '_', '+')
