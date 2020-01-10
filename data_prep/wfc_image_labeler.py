55341fd404f382fc5a067c333ab237337c360d3c
neronicolo <neronicolo@gmail.com>
2020-01-09 17:21:06 -0800

    added 2 options for image size

--- data_prep/wfc_image_labeler.py
+++ data_prep/wfc_image_labeler.py
@@ -33,7 +33,8 @@ class Application(tk.Frame):
         self.csv_file_path = Path(csv_file_path).resolve(strict=True)
         self._index = -1
         self._init_start = 1
-        self.size = (640, 640)
+        #self.size = (640, 640)
+        self.size = (480, 480)
         self.df = pd.read_csv(self.imgs_dir_path/self.csv_file_path)
         self.df_filtered = self.df.copy()
         
