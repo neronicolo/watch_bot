4a922675755a5fbbb228f6cf8d48395138d6ac47
neronicolo <neronicolo@gmail.com>
2020-03-23 22:09:02 -0700

    RENAMING FILES

--- inference/dial_visibility_prediction.py
+++ inference/dial_visibility_prediction.py
@@ -5,12 +5,12 @@ from tqdm import tqdm
 #   file_attrib(source folder(starting folder for recursion), csv_path(where to save), **kwargs(column for labelin))
 
 data_path =  Path.home()/'programming/data/chrono24/'
-csv_path = data_path/'file_attribs_inference.csv'
+csv_path = data_path/'dial_visibility_inference.csv'
 
 files = sorted(data_path.rglob('*.jpg'))
 file_attribs = defaultdict(list)
 
-learn = load_learner(data_path)
+learn = load_learner(data_path, file='dial_visibility_export.pkl')
 learn = learn.to_fp32()
 
 for file in tqdm(files):
@@ -23,9 +23,6 @@ for file in tqdm(files):
     file_attribs['dial_visibility'].append(p_idx.tolist())
     file_attribs['dial_visibility_p_0'].append(p.tolist()[0])
     file_attribs['dial_visibility_p_1'].append(p.tolist()[1])
-    file_attribs['like'].append(-1)
-    file_attribs['like_p_0'].append(-1)
-    file_attribs['like_p_1'].append(-1)
 
 df = pd.DataFrame.from_dict(file_attribs)
 df.to_csv(csv_path, index=False)
