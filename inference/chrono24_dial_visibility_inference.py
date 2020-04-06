93bc6903c6b3e58acf1f88f219a3475a7c3efbd0
neronicolo <neronicolo@gmail.com>
2020-04-05 20:11:10 -0700

    changing csv path name

--- inference/chrono24_dial_visibility_inference.py
+++ inference/chrono24_dial_visibility_inference.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 #   file_attrib(source folder(starting folder for recursion), csv_path(where to save), **kwargs(column for labelin))
 
 data_path =  Path.home()/'programming/data/chrono24/'
-csv_path = data_path/'dial_visibility_inference.csv'
+csv_path = data_path/'chrono24_dial_visibility_inference.csv'
 
 files = sorted(data_path.rglob('*.jpg'))
 file_attribs = defaultdict(list)
